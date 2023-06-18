da974742363fcdaed820a5a918f7a8a445a54462
Morgan Sziraki <morgan.sziraki@gmail.com>
2023-06-18 09:50:51 +0100

    DEV-001 correct comment and add better help

--- bin/mac/network_password.sh
+++ bin/mac/network_password.sh
@@ -4,7 +4,8 @@ if [ -n "$1" ]; then
   security find-generic-password -D "AirPort network password" -a "$1" -gw
 else
   echo "Usage: "
-  echo "  access_points.sh SSID"
-  echo "Where SSID = the AP you want to query"
+  echo "  $0 SSID"
+  echo "Where SSID = A wireless network that you have previously been authenticated on."
+	echo "             You will be prompted for an Administrator user name and password."
   exit 1 
 fi
